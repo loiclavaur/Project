83982a5189a6b5cef7c086e386e5e2fbe1520061
loiclavaur <105443465+loiclavaur@users.noreply.github.com>
2022-05-12 17:13:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="author" content="NoS1gnal"/>
+
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+			<link rel ="stylesheet" type="text/css" href="css/style.css">
+            <title>Connexion</title>
+        </head>
+        <body>
+		
+		<header>
+			<a href="menu.html" class="logo">Projet</a>
+			<ul>
+				<li><a href="menu.html">Accueil</a></li>
+				<li><a href="robot.html">Robot</a></li>
+				<li><a href="index.php">Connexion</a></li>
+				<a href="menu.html">
+					<img height="30px" width="30px" src="css/image/fr.png"/>
+				</a>
+				<li><a href="#">  </a></li>
+				<a href="espagnol.html">
+					<img height="30px" width="30px" src="css/image/spain.png"/>
+				</a>
+			</ul>
+		</header>
+        
+        <div class="login-form">
+             <?php 
+                if(isset($_GET['login_err']))
+                {
+                    $err = htmlspecialchars($_GET['login_err']);
+
+                    switch($err)
+                    {
+                        case 'password':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> mot de passe incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'already':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> compte non existant
+                            </div>
+                        <?php
+                        break;
+                    }
+                }
+                ?> 
+            
+            <form action="connexion.php" method="post">
+                <h2 class="text-center">Connexion</h2>       
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">Connexion</button>
+                </div>   
+            </form>
+            <p class="text-center"><a href="inscription.php">Inscription</a></p>
+        </div>
+      
+        </body>
+</html>
